4e8ca8888a7060e20246436e900fd04aafb2e696
meloy <10772794+marcupan@users.noreply.github.com>
2025-11-17 20:15:07 +0200

    update configuration

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,7 +1,19 @@
-import type { NextConfig } from "next";
+import type {NextConfig} from "next";
+import path from 'path';
 
 const nextConfig: NextConfig = {
-  /* config options here */
+    /* config options here */
+    transpilePackages: ['graphql', '@apollo/client', 'graphql-ws'],
+    webpack: (config, {isServer}) => {
+        config.resolve.alias = {
+            ...config.resolve.alias,
+            graphql: path.resolve('./node_modules/graphql'),
+        };
+        return config;
+    },
+    turbopack: {
+        root: __dirname,
+    },
 };
 
 export default nextConfig;
