98670a1bd245b91be3b60c838dd63e707a12cdfb
meloy <10772794+marcupan@users.noreply.github.com>
2025-12-11 20:12:27 +0200

    update apollo provider

--- frontend/components/ApolloProvider.tsx
+++ frontend/components/ApolloProvider.tsx
@@ -1,17 +1,17 @@
 'use client';
 
-import { ApolloProvider as BaseApolloProvider } from '@apollo/client';
+import {ApolloProvider as BaseApolloProvider} from '@apollo/client/react';
 import client from '../lib/apollo-client';
-import { ReactNode } from 'react';
+import {ReactNode} from 'react';
 
 interface ApolloProviderProps {
-  children: ReactNode;
+    children: ReactNode;
 }
 
-export default function ApolloProvider({ children }: ApolloProviderProps) {
-  return (
-    <BaseApolloProvider client={client}>
-      {children}
-    </BaseApolloProvider>
-  );
+export default function ApolloProvider({children}: ApolloProviderProps) {
+    return (
+        <BaseApolloProvider client={client}>
+            {children}
+        </BaseApolloProvider>
+    );
 }
