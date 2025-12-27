1f27dcf49cac226e9b1766bc77258a2a94441fb4
meloy <10772794+marcupan@users.noreply.github.com>
2025-12-27 19:34:45 +0200

    update jest config

--- jest.config.js
+++ jest.config.js
@@ -1,16 +1,16 @@
 export default {
-    preset: 'ts-jest/presets/default-esm',
-    testEnvironment: 'node',
-    transform: {
-        '^.+\\.ts?$': [
-            'ts-jest',
-            {
-                useESM: true,
-            },
-        ],
-    },
-    moduleNameMapper: {
-        '^(\\.{1,2}/.*)\\.js$': '$1',
-    },
-    testPathIgnorePatterns: ['/node_modules/', '/dist/'],
+  preset: 'ts-jest/presets/default-esm',
+  testEnvironment: 'node',
+  transform: {
+    '^.+\\.ts?$': [
+      'ts-jest',
+      {
+        useESM: true,
+      },
+    ],
+  },
+  moduleNameMapper: {
+    '^(\\.{1,2}/.*)\\.js$': '$1',
+  },
+  testPathIgnorePatterns: ['/node_modules/', '/dist/'],
 };
