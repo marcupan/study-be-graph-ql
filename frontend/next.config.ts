20adc663814f695b89e6e79010926f9bc986922b
dzhovtobryukh <10772794+marcupan@users.noreply.github.com>
2026-02-06 22:00:10 +0200

    update frontend configuration

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,20 +1,10 @@
-import path from 'path';
+// @ts-check
 
-import type {NextConfig} from "next";
-
-const nextConfig: NextConfig = {
-    /* config options here */
-    transpilePackages: ['graphql', '@apollo/client', 'graphql-ws'],
-    webpack: (config, {isServer}) => {
-        config.resolve.alias = {
-            ...config.resolve.alias,
-            graphql: path.resolve('./node_modules/graphql'),
-        };
-        return config;
-    },
-    turbopack: {
-        root: __dirname,
-    },
+const nextConfig = {
+  webpack: (config) => {
+    config.externals.push('pino-pretty', 'lokijs', 'encoding');
+    return config;
+  },
 };
 
 export default nextConfig;
