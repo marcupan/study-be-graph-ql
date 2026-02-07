7d2f906d18c31245b455cae8c340b3d73a273c87
dzhovtobryukh <10772794+marcupan@users.noreply.github.com>
2026-02-07 17:52:48 +0200

    update frontend eslint

---
+++ frontend/eslint.config.js
@@ -0,0 +1,23 @@
+// @ts-check
+
+import eslint from '@eslint/js';
+import tseslint from 'typescript-eslint';
+import nextPlugin from '@next/eslint-plugin-next';
+
+export default tseslint.config(
+  eslint.configs.recommended,
+  ...tseslint.configs.recommended,
+  {
+    plugins: {
+      '@next/next': nextPlugin,
+    },
+    rules: {
+      ...nextPlugin.configs.recommended.rules,
+      ...nextPlugin.configs['core-web-vitals'].rules,
+      '@typescript-eslint/no-unused-vars': ['error', { argsIgnorePattern: '^_', varsIgnorePattern: '^_' }],
+    },
+  },
+  {
+    ignores: ['.next/'],
+  },
+);
